28fec03a59d5653ae2fe3da9f87e0fbcfe55e2c5
Rodrigo Lourenco <rodrigo.s.lourenco@gmail.com>
2014-07-22 16:31:12 -0300

    Retornando com validação de svg no browser corrente

--- dist/js/glb.share.js
+++ dist/js/glb.share.js
@@ -58,7 +58,7 @@ if (window.glb === undefined) {
 
 
         hasSupportSvg: function hasSupportSvg() {
-            return false;
+            return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1");
         },
 
         createSVG: function createSVG() {
