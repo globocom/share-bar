7a0e7cee7f4892c9c59bddd2a7cce9a8a280f448
Romulo Tavares <romulo.tavares@corp.globo.com>
2014-09-16 22:01:28 -0300

    Changed config of coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -144,14 +144,12 @@ module.exports = function (grunt) {
         },
 
         coveralls: {
-            options: {
-              // LCOV coverage file relevant to every target
-              src: 'tests/reports/lcov.info',
 
-              // When true, grunt-coveralls will only print a warning rather than
-              // an error, to prevent CI builds from failing unnecessarily (e.g. if
-              // coveralls.io is down). Optional, defaults to false.
-              force: false
+            options: {
+              force: true
+            },
+            main_target: {
+                src: "tests/reports/lcov.info"
             }
         },
 
